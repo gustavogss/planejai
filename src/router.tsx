d6d7743237fccedfbc8b7d3733eb6c7393c33e1f
gustavogss <gustavogss.dev@gmail.com>
2026-05-30 16:49:10 -0300

    feat: create header

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { RootLayout } from '@/components/layout/RootLayout'
 import { History } from '@/pages/History'
 import Home from '@/pages/Home'
 import { Result } from '@/pages/Result'
 
 export const router = createBrowserRouter([
   {
+    element: <RootLayout />,
     children: [
       { path: '/', element: <Home /> },
       { path: '/resultado', element: <Result /> },
